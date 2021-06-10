626f1d49db2ff93eb62eb4258e1419d074707f5b
Yoonsoo Kim <ykim7@caltech.edu>
2021-06-10 16:59:35 +0900

    Add Krivodonova analytic sol

---
+++ src/PointwiseFunctions/AnalyticSolutions/ScalarAdvection/Krivodonova.cpp
@@ -0,0 +1,93 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "PointwiseFunctions/AnalyticSolutions/ScalarAdvection/Krivodonova.hpp"
+
+#include <cmath>
+#include <cstddef>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Utilities/ContainerHelpers.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/MakeWithValue.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace ScalarAdvection::Solutions {
+
+template <typename DataType>
+tuples::TaggedTuple<ScalarAdvection::Tags::U> Krivodonova::variables(
+    const tnsr::I<DataType, 1>& x, double t,
+    tmpl::list<ScalarAdvection::Tags::U> /*meta*/) const noexcept {
+  // map each points back to its position at t=0
+  auto x0 = make_with_value<tnsr::I<DataType, 1>>(x, 0.0);
+  get<0>(x0) = get<0>(x) - t;
+
+  for (size_t i = 0; i < get_size(get<0>(x)); ++i) {
+    auto& xi = get<0>(x0)[i];
+    xi = xi - 2.0 * floor(0.5 * (xi + 1.0));
+  }
+
+  const double a{0.5};
+  const double z{-0.7};
+  const double delta{0.005};
+  const double alpha{10.0};
+  const double beta{log(2.0) / (36.0 * pow(delta, 2.0))};
+
+  const auto F = [](const double x_var, const double alpha_var,
+                    const double a_var) noexcept {
+    return sqrt(fmax(1.0 - pow(alpha_var, 2.0) * pow(x_var - a_var, 2.0), 0.0));
+  };
+  const auto G = [](const double x_var, const double beta_var,
+                    const double z_var) noexcept {
+    return exp(-beta_var * pow(x_var - z_var, 2.0));
+  };
+
+  auto u_variable = make_with_value<Scalar<DataType>>(x0, 0.0);
+  for (size_t i = 0; i < get_size(get<0>(x0)); ++i) {
+    const auto& xi = get<0>(x0)[i];
+    auto& ui = get(u_variable)[i];
+
+    if ((-0.8 <= xi) and (xi <= -0.6)) {
+      ui = (G(xi, beta, z - delta) + G(xi, beta, z + delta) +
+            4.0 * G(xi, beta, z)) /
+           6.0;
+    } else if ((-0.4 <= xi) and (xi <= -0.2)) {
+      ui = 1.0;
+    } else if ((0.0 <= xi) and (xi <= 0.2)) {
+      ui = 1.0 - abs(10.0 * xi - 1.0);
+    } else if ((0.4 <= xi) and (xi <= 0.6)) {
+      ui = (F(xi, alpha, a - delta) + F(xi, alpha, a + delta) +
+            4.0 * F(xi, alpha, a)) /
+           6.0;
+    } else {
+      ui = 0.0;
+    }
+  }
+  return u_variable;
+}
+
+void Krivodonova::pup(PUP::er& /*p*/) noexcept {}
+
+bool operator==(const Krivodonova& /*lhs*/,
+                const Krivodonova& /*rhs*/) noexcept {
+  return true;
+}
+
+bool operator!=(const Krivodonova& lhs, const Krivodonova& rhs) noexcept {
+  return not(lhs == rhs);
+}
+
+}  // namespace ScalarAdvection::Solutions
+
+#define DTYPE(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATE(_, data)                             \
+  template tuples::TaggedTuple<ScalarAdvection::Tags::U> \
+  ScalarAdvection::Solutions::Krivodonova::variables(    \
+      const tnsr::I<DTYPE(data), 1>& x, double t,        \
+      tmpl::list<ScalarAdvection::Tags::U> /*meta*/) const noexcept;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (DataVector))
+
+#undef DTYPE
+#undef INSTANTIATE
