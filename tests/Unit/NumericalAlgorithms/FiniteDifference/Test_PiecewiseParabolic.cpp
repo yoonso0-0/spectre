bf3dc0bd8173a95fd1fb4ace1f4b9f15e6c698df
Yoonsoo Kim <ykim7@caltech.edu>
2022-06-22 15:25:31 -0700

    Add skeleton for PPM reconstruction

---
+++ tests/Unit/NumericalAlgorithms/FiniteDifference/Test_PiecewiseParabolic.cpp
@@ -0,0 +1,19 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+namespace fd::reconstruction {
+
+namespace {
+
+void test() {}
+
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.FiniteDifference.PiecewiseParabolic",
+                  "[Unit][NumericalAlgorithms]") {
+  test();
+}
+
+}  // namespace fd::reconstruction
