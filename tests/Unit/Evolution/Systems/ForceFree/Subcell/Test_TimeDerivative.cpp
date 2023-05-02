6b9592940c362e8df25be654b73c0c9de3cf67d2
Yoonsoo Kim <ykim7@caltech.edu>
2023-05-01 17:41:30 -0700

    Add the ForceFree subcell time derivative

---
+++ tests/Unit/Evolution/Systems/ForceFree/Subcell/Test_TimeDerivative.cpp
@@ -0,0 +1,19 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include "Evolution/Systems/ForceFree/Subcell/TimeDerivative.hpp"
+
+namespace ForceFree {
+namespace {
+
+void test() {}
+
+SPECTRE_TEST_CASE("Unit.Evolution.Systems.ForceFree.Subcell.TimeDerivative",
+                  "[Unit][Evolution]") {
+  test();
+}
+
+}  // namespace
+}  // namespace ForceFree
