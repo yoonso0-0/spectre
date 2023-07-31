309db52249646481e14ca954f6629e443d5af635
Yoonsoo Kim <ykim7@caltech.edu>
2023-07-31 11:37:53 -0700

    Add SwapMask

---
+++ src/Evolution/Systems/ForceFree/Subcell/SwapMask.hpp
@@ -0,0 +1,36 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "Utilities/TMPL.hpp"
+
+#include "Domain/Tags.hpp"
+#include "Evolution/DgSubcell/ActiveGrid.hpp"
+#include "Evolution/DgSubcell/Tags/ActiveGrid.hpp"
+#include "Evolution/DgSubcell/Tags/Inactive.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
+#include "Evolution/Systems/ForceFree/Tags.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace ForceFree::subcell {
+
+/*!
+ * \brief Swap (why)
+ *
+ */
+struct SwapMask {
+  using return_tags = tmpl::list<
+      Tags::NsInteriorMask,
+      evolution::dg::subcell::Tags::Inactive<Tags::NsInteriorMask>>;
+  using argument_tags =
+      tmpl::list<::domain::Tags::Mesh<3>, evolution::dg::subcell::Tags::Mesh<3>,
+                 evolution::dg::subcell::Tags::ActiveGrid>;
+
+  static void apply(gsl::not_null<Scalar<DataVector>*> active_mask,
+                    gsl::not_null<Scalar<DataVector>*> inactive_mask,
+                    const Mesh<3>& dg_mesh, const Mesh<3>& subcell_mesh,
+                    evolution::dg::subcell::ActiveGrid active_grid);
+};
+
+}  // namespace ForceFree::subcell
