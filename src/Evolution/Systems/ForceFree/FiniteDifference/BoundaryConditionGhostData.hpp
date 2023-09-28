e11199482f1fb94b151949813cdbda3dd600a5e4
Yoonsoo Kim <ykim7@caltech.edu>
2023-09-28 15:41:52 -0700

    [wip] Add subcell BC

---
+++ src/Evolution/Systems/ForceFree/FiniteDifference/BoundaryConditionGhostData.hpp
@@ -0,0 +1,196 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+#include <optional>
+#include <type_traits>
+#include <unordered_set>
+#include <utility>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Variables.hpp"
+#include "Domain/BoundaryConditions/BoundaryCondition.hpp"
+#include "Domain/BoundaryConditions/None.hpp"
+#include "Domain/BoundaryConditions/Periodic.hpp"
+#include "Domain/Creators/Tags/ExternalBoundaryConditions.hpp"
+#include "Domain/Domain.hpp"
+#include "Domain/Structure/Direction.hpp"
+#include "Domain/Structure/Element.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Tags.hpp"
+#include "Domain/TagsTimeDependent.hpp"
+#include "Evolution/BoundaryConditions/Type.hpp"
+#include "Evolution/DgSubcell/Tags/CellCenteredFlux.hpp"
+#include "Evolution/DgSubcell/Tags/GhostDataForReconstruction.hpp"
+#include "Evolution/DgSubcell/Tags/Mesh.hpp"
+#include "Evolution/DiscontinuousGalerkin/NormalVectorTags.hpp"
+#include "Evolution/Systems/ForceFree/BoundaryConditions/BoundaryCondition.hpp"
+#include "Evolution/Systems/ForceFree/BoundaryConditions/Factory.hpp"
+#include "Evolution/Systems/ForceFree/FiniteDifference/Reconstructor.hpp"
+#include "Evolution/Systems/ForceFree/FiniteDifference/Tags.hpp"
+#include "Evolution/Systems/ForceFree/System.hpp"
+#include "Evolution/Systems/ForceFree/Tags.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "Parallel/Tags/Metavariables.hpp"
+#include "PointwiseFunctions/Hydro/Tags.hpp"
+#include "Utilities/CallWithDynamicType.hpp"
+#include "Utilities/ErrorHandling/Assert.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/PrettyType.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace ForceFree::fd {
+
+/*!
+ * \brief Computes finite difference ghost data for external boundary
+ * conditions.
+ *
+ * If the element is at the external boundary, computes FD ghost data with a
+ * given boundary condition and stores it into neighbor data with {direction,
+ * ElementId::external_boundary_id()} as the mortar_id key.
+ *
+ * \note Subcell needs to be enabled for boundary elements. Otherwise this
+ * function would be never called.
+ *
+ */
+struct BoundaryConditionGhostData {
+  template <typename DbTagsList>
+  static void apply(const gsl::not_null<db::DataBox<DbTagsList>*> box,
+                    const Element<3>& element,
+                    const Reconstructor& reconstructor);
+
+ private:
+  template <typename FdBoundaryConditionHelper, typename DbTagsList,
+            typename... FdBoundaryConditionArgsTags>
+  // A helper function for calling fd_ghost() of BoundaryCondition subclasses
+  static void apply_subcell_boundary_condition_impl(
+      FdBoundaryConditionHelper& fd_boundary_condition_helper,
+      const gsl::not_null<db::DataBox<DbTagsList>*>& box,
+      tmpl::list<FdBoundaryConditionArgsTags...>) {
+    return fd_boundary_condition_helper(
+        db::get<FdBoundaryConditionArgsTags>(*box)...);
+  }
+};
+
+template <typename DbTagsList>
+void BoundaryConditionGhostData::apply(
+    const gsl::not_null<db::DataBox<DbTagsList>*> box,
+    const Element<3>& element, const Reconstructor& reconstructor) {
+  const auto& external_boundary_condition =
+      db::get<domain::Tags::ExternalBoundaryConditions<3>>(*box).at(
+          element.id().block_id());
+
+  // Check if the element is on the external boundary. If not, the caller is
+  // doing something wrong (e.g. trying to compute FD ghost data with boundary
+  // conditions at an element which is not on the external boundary).
+  ASSERT(not element.external_boundaries().empty(),
+         "The element (ID : " << element.id()
+                              << ") is not on external boundaries");
+
+  const Mesh<3> subcell_mesh =
+      db::get<evolution::dg::subcell::Tags::Mesh<3>>(*box);
+
+  const size_t ghost_zone_size{reconstructor.ghost_zone_size()};
+
+  size_t num_reconstructed_tensor_components = 0;
+  tmpl::for_each<ForceFree::fd::tags_list_for_reconstruction>(
+      [&num_reconstructed_tensor_components](auto tag) {
+        num_reconstructed_tensor_components +=
+            tmpl::type_from<decltype(tag)>::type::size();
+      });
+
+  for (const auto& direction : element.external_boundaries()) {
+    const auto& boundary_condition_at_direction =
+        *external_boundary_condition.at(direction);
+
+    const size_t num_face_pts{
+        subcell_mesh.extents().slice_away(direction.dimension()).product()};
+
+    // Allocate a vector to store the computed FD ghost data and assign a
+    // non-owning Variables on it.
+    auto& all_ghost_data = db::get_mutable_reference<
+        evolution::dg::subcell::Tags::GhostDataForReconstruction<3>>(box);
+    // Put the computed ghost data into neighbor data with {direction,
+    // ElementId::external_boundary_id()} as the mortar_id key
+    const DirectionalId<3> mortar_id{direction,
+                                     ElementId<3>::external_boundary_id()};
+
+    all_ghost_data[mortar_id] = evolution::dg::subcell::GhostData{1};
+    DataVector& boundary_ghost_data =
+        all_ghost_data.at(mortar_id).neighbor_ghost_data_for_reconstruction();
+    boundary_ghost_data.destructive_resize(num_reconstructed_tensor_components *
+                                           ghost_zone_size * num_face_pts);
+    Variables<ForceFree::fd::tags_list_for_reconstruction> ghost_data_vars{
+        boundary_ghost_data.data(), boundary_ghost_data.size()};
+
+    // We don't need to care about boundary ghost data when using the periodic
+    // condition, so exclude it from the type list
+    using factory_classes =
+        typename std::decay_t<decltype(db::get<Parallel::Tags::Metavariables>(
+            *box))>::factory_creation::factory_classes;
+    using derived_boundary_conditions_for_subcell = tmpl::remove_if<
+        tmpl::at<factory_classes, typename System::boundary_conditions_base>,
+        tmpl::or_<
+            std::is_base_of<domain::BoundaryConditions::MarkAsPeriodic,
+                            tmpl::_1>,
+            std::is_base_of<domain::BoundaryConditions::MarkAsNone, tmpl::_1>>>;
+
+    // Now apply subcell boundary conditions
+    call_with_dynamic_type<void, derived_boundary_conditions_for_subcell>(
+        &boundary_condition_at_direction,
+        [&box, &direction, &ghost_data_vars](const auto* boundary_condition) {
+          using BoundaryCondition = std::decay_t<decltype(*boundary_condition)>;
+          using bcondition_interior_evolved_vars_tags =
+              typename BoundaryCondition::fd_interior_evolved_variables_tags;
+          using bcondition_interior_temporary_tags =
+              typename BoundaryCondition::fd_interior_temporary_tags;
+          using bcondition_gridless_tags =
+              typename BoundaryCondition::fd_gridless_tags;
+
+          using bcondition_interior_tags =
+              tmpl::append<bcondition_interior_evolved_vars_tags,
+                           bcondition_interior_temporary_tags,
+                           bcondition_gridless_tags>;
+
+          if constexpr (BoundaryCondition::bc_type ==
+                        evolution::BoundaryConditions::Type::Ghost) {
+            const auto apply_fd_ghost =
+                [&boundary_condition, &direction,
+                 &ghost_data_vars](const auto&... boundary_ghost_data_args) {
+                  (*boundary_condition)
+                      .fd_ghost(
+                          make_not_null(
+                              &get<ForceFree::Tags::TildeJ>(ghost_data_vars)),
+                          make_not_null(
+                              &get<ForceFree::Tags::TildeE>(ghost_data_vars)),
+                          make_not_null(
+                              &get<ForceFree::Tags::TildeB>(ghost_data_vars)),
+                          make_not_null(
+                              &get<ForceFree::Tags::TildePsi>(ghost_data_vars)),
+                          make_not_null(
+                              &get<ForceFree::Tags::TildePhi>(ghost_data_vars)),
+                          make_not_null(
+                              &get<ForceFree::Tags::TildeQ>(ghost_data_vars)),
+                          direction, boundary_ghost_data_args...);
+                };
+            apply_subcell_boundary_condition_impl(apply_fd_ghost, box,
+                                                  bcondition_interior_tags{});
+          } else if constexpr (BoundaryCondition::bc_type ==
+                               evolution::BoundaryConditions::Type::
+                                   DemandOutgoingCharSpeeds) {
+            //
+            //
+            //
+          } else {
+            ERROR("Unsupported boundary condition "
+                  << pretty_type::short_name<BoundaryCondition>()
+                  << " when using finite-difference");
+          }
+        });
+  }
+}
+
+}  // namespace ForceFree::fd
